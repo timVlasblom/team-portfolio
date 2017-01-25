584a413bfa877e5c699dfbd7f5cd81b45a6f001c
Tim Vlasblom <Tim Vlasblom>
2017-01-25 14:29:36 +0100

    added list to img's

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -2,11 +2,13 @@
 <section class="portfolio">
   <h2>portfolio</h2>
   <p class="portfolio-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <ul>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+  </ul>
   <div class="mini-footer"></div>
 </section>
