810abc38234005a68dfb82562d733e740fa92b64
Tim Vlasblom <Tim Vlasblom>
2017-01-25 13:50:14 +0100

    removed h2 class

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,11 +1,12 @@
 
 <section class="portfolio">
-  <h2 class="portfolio-title">portfolio</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
+  <h2>portfolio</h2>
+  <p class="portfolio-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
   <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
   <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
   <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
   <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
   <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
   <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <div class="mini-footer"></div>
 </section>
