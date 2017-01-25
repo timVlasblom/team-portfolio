c446eb92c18c9f4c2f1df4429a010477259087d4
Peter Born <peter@Peters-MacBook-Pro.local>
2017-01-25 15:16:33 +0100

    made the css for contact

--- templates/contact.php
+++ templates/contact.php
@@ -1,9 +1,9 @@
     <section class="contact">
         <h2>contact</h2>
         <form class="contact-form">
-          <input type="text" name="name" placeholder="Name...">
-          <input type="text" name="email" placeholder="Email...">
-          <textarea placeholder="Message..."></textarea>
+          <input class="name" type="text" name="name" placeholder="Name...">
+          <input class="email" type="text" name="email" placeholder="Email...">
+          <textarea class="message" placeholder="Message..."></textarea>
           <input class="send" type="submit" value="send">
         </form>
     </section>
