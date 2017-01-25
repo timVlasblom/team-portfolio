7bc5b41a4f9e589e0cc3c69241bb400bd0fc45fe
Peter Born <peter@Peters-MacBook-Pro.local>
2017-01-25 11:39:49 +0100

    html added

---
+++ templates/contact.php
@@ -0,0 +1,9 @@
+    <section class="contact">
+        <h2>contact</h2>
+        <form class="send-me">
+          <input type="text" name="name" placeholder="Name...">
+          <input type="text" name="email" placeholder="Email...">
+          <textarea placeholder="Message..."></textarea>
+          <input type="submit" value="send">
+        </form>
+    </section>
