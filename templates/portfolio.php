34d2152ad1ce48b18dee33626646177c6e59b0a4
Sem van der Schilt <semvanderschilt@Sems-MacBook-Pro.local>
2017-01-25 15:27:14 +0100

    reverted commit backup html

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,14 +1,11 @@
 
 <section class="portfolio">
-  <h2>portfolio</h2>
-  <p class="portfolio-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
-  <ul>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-  </ul>
-  <div class="mini-footer"></div>
+  <h2 class="portfolio-title">portfolio</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
 </section>
