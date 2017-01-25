6877c076c6bdc58f7f0f0c6dba029e61a814ead9
Koen Kort <koenkort@MacBook-Air-van-Koen.local>
2017-01-25 13:11:55 +0100

    made basic html for about section

---
+++ templates/about.php
@@ -0,0 +1,12 @@
+<section class="about">
+  <h2>About</h2>
+
+  <div class="about-paragraphs">
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+    <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+    <img src="img/cat_with_a_hat.jpg">
+    <img src="img/cat_with_a_hat.jpg">
+  </div>
+
+  <img class="about-profile-img" src="img/cat_with_a_hat.jpg">
+</section>
