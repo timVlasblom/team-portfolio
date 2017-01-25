b3ce24c0855006eaf828d2d21ab2dd3fc134a3e4
Sem van der Schilt <semvanderschilt@Sems-MacBook-Pro.local>
2017-01-25 13:50:31 +0100

    addit templates banner.php, main-nav.php

---
+++ templates/banner.php
@@ -0,0 +1,10 @@
+<section class="lead-banner">
+  <img src="img/banner-1.jpg" />
+  <div class="banner-content">
+    <div class="wrapper">
+      <h1>JOHN DOE</h1>
+      <h2>FRONT-END DEVELOPER</h2>
+      <input type="submit" value="Learn more">
+    </div>
+  </div>
+</section>
