b93f96b05fe7bdb79af0e0a560e3cb07594b9c1b
Tim Vlasblom <Tim Vlasblom>
2017-01-25 09:10:23 +0100

    made folder structure

---
+++ templates/portfolio.php
@@ -0,0 +1,11 @@
+
+<section class="portfolio">
+  <h2 class="portfolio-title">portfolio</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+</section>
