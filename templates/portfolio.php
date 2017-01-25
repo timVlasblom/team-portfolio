40d4df8627b96d4f1b96ba8a2c364940d66660b7
Sem van der Schilt <semvanderschilt@Sems-MacBook-Pro.local>
2017-01-25 15:23:27 +0100

    unwanted merge

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,11 +1,14 @@
 
 <section class="portfolio">
-  <h2 class="portfolio-title">portfolio</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
-  <img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat">
+  <h2>portfolio</h2>
+  <p class="portfolio-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
+  <ul>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+  </ul>
+  <div class="mini-footer"></div>
 </section>
