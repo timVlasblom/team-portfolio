3c93b0208d5f0f59e3de304b5df4bc3f32d21eea
Maxime Koomen <Maxime@Macbook.local>
2017-01-25 13:10:40 +0100

    made the html

---
+++ templates/skills.php
@@ -0,0 +1,15 @@
+
+    <section class="skills">
+      <h3>Skills</h3>
+      <ul>
+        <li>photoshop</li>
+        <li>Illustrator</li>
+        <li>Sketch</li>
+        <li>Html (5)</li>
+        <li>Css (3)</li>
+        <li>Sass</li>
+        <li>Javascript</li>
+        <li>Gulp</li>
+        <li>Git</li>
+      </ul>
+    </section>
