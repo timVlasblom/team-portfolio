59b7f7fb11d048ebd28824887cdf9eca8fcb6106
Koen Kort <koenkort@MacBook-Air-van-Koen.local>
2017-01-25 15:16:47 +0100

    edited the html file and made it a proper structure, made the about css file

--- templates/about.php
+++ templates/about.php
@@ -4,8 +4,10 @@
   <div class="about-paragraphs">
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
     <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
-    <img src="img/cat_with_a_hat.jpg">
-    <img src="img/cat_with_a_hat.jpg">
+    <div class="socialmedia">
+      <img class="linkedin"src="img/cat_with_a_hat.jpg">
+      <img class="git" src="img/cat_with_a_hat.jpg">
+    </div>
   </div>
 
   <img class="about-profile-img" src="img/cat_with_a_hat.jpg">
