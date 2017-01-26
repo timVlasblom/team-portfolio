834934dee4e054ba0e9e20aa971ddc6fc2204966
Tim Vlasblom <Tim Vlasblom>
2017-01-26 11:33:11 +0100

    made the pictures show text on hover

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -2,13 +2,43 @@
 <section class="portfolio">
   <h2>portfolio</h2>
   <p class="portfolio-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
-  <ul>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
-    <li><img src="img/cat_with_a_hat.jpg" alt="cat" class="cat_with_a_hat"></li>
+  <ul class="img-list">
+    <li>
+      <a href="/index.php">
+        <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
+        <span class="text-content"><span>Place Name</span></span>
+      </a>
+    </li>
+    <li>
+      <a href="/index.php">
+        <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
+        <span class="text-content"><span>Place Name</span></span>
+      </a>
+    </li>
+    <li>
+      <a href="/index.php">
+        <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
+        <span class="text-content"><span>Place Name</span></span>
+      </a>
+    </li>
+    <li>
+      <a href="/index.php">
+        <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
+        <span class="text-content"><span>Place Name</span></span>
+      </a>
+    </li>
+    <li>
+      <a href="/index.php">
+        <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
+        <span class="text-content"><span>Place Name</span></span>
+      </a>
+    </li>
+    <li>
+      <a href="/index.php">
+        <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
+        <span class="text-content"><span>Place Name</span></span>
+      </a>
+    </li>
   </ul>
   <div class="mini-footer"></div>
 </section>
