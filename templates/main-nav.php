d1c74e5eab6f14b2691b3ae5106a612f2cf4bfba
Sem van der Schilt <semvanderschilt@Sems-MacBook-Pro.local>
2017-01-26 16:13:14 +0100

    added the nav

--- templates/main-nav.php
+++ templates/main-nav.php
@@ -1,9 +1,9 @@
 <nav class="navigation">
   <ul class="nav-links">
-    <li><a href="/home/">Home</a></li>
-    <li><a href="/about/">About</a></li>
-    <li><a href="/skills/">Skills</a></li>
-    <li><a href="/portfolio/">Portfolio</a></li>
-    <li><a href="/contact/">Contact</a></li>
+    <li class="nav-text"><a href="/home/">Home</a></li>
+    <li class="nav-text"><a href="/about/">About</a></li>
+    <li class="nav-text"><a href="/skills/">Skills</a></li>
+    <li class="nav-text"><a href="/portfolio/">Portfolio</a></li>
+    <li class="nav-text"><a href="/contact/">Contact</a></li>
   </ul>
 </nav>
