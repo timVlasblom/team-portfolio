68b09b737bde7a730638a2139d74dc726b77d4ab
Sem van der Schilt <semvanderschilt@Sems-MacBook-Pro.local>
2017-01-26 15:31:27 +0100

    finish banner

--- templates/banner.php
+++ templates/banner.php
@@ -3,5 +3,8 @@
   <div class="banner-content">
     <h1>JOHN DOE</h1>
     <h2>FRONT-END DEVELOPER</h2>
+    <a class="banner-button" href="http://groenst.com/">LEARN MORE</a>
+    <div class="button-shape"></div>
   </div>
+  <div class="mini-footer"></div>
 </section>
