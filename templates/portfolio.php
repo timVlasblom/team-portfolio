3713a31757829b3f85930e0380ae5a5cfd6fe993
Tim Vlasblom <Tim Vlasblom>
2017-01-26 15:04:40 +0100

    made final patches

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -5,38 +5,50 @@
   <ul class="img-list">
     <li>
       <a href="/index.php">
+        <div class="image-overlay">
         <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
-        <span class="text-content"><span>Place Name</span></span>
+        <span class="text-content">Place Name</span>
+      </div>
       </a>
     </li>
     <li>
       <a href="/index.php">
+        <div class="image-overlay">
         <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
-        <span class="text-content"><span>Place Name</span></span>
+        <span class="text-content">Place Name</span>
+      </div>
       </a>
     </li>
     <li>
       <a href="/index.php">
+        <div class="image-overlay">
         <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
-        <span class="text-content"><span>Place Name</span></span>
+        <span class="text-content">Place Name</span>
+      </div>
       </a>
     </li>
     <li>
       <a href="/index.php">
+        <div class="image-overlay">
         <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
-        <span class="text-content"><span>Place Name</span></span>
+        <span class="text-content">Place Name</span>
+      </div>
       </a>
     </li>
     <li>
       <a href="/index.php">
+        <div class="image-overlay">
         <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
-        <span class="text-content"><span>Place Name</span></span>
+        <span class="text-content">Place Name</span>
+      </div>
       </a>
     </li>
     <li>
       <a href="/index.php">
+        <div class="image-overlay">
         <img  class="cat_with_a_hat" src="img/cat_with_a_hat.jpg"></img>
-        <span class="text-content"><span>Place Name</span></span>
+        <span class="text-content">Place Name</span>
+      </div>
       </a>
     </li>
   </ul>
