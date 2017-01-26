45afb20d1053caf75fd766d54825849f467e88be
Maxime Koomen <Maxime@Macbook.local>
2017-01-26 10:21:49 +0100

    changed the text

--- templates/skills.php
+++ templates/skills.php
@@ -3,15 +3,16 @@
       <h3>SKILLS</h3>
       <ul class="skill-list">
         <li>Photoshop</li>
-        <li>Illustrator</li>
-        <li>Sketch</li>
         <li>Html (5)</li>
-        <li>Css (3)</li>
-        <li>Sass</li>
         <li>Javascript</li>
+        <li>Illustrator</li>
+        <li>Css (3)</li>
         <li>Gulp</li>
+        <li>Sketch</li>
+        <li>Sass</li>
         <li>Git</li>
+        <hr></hr>
       </ul>
-      <div class="mini-footer">
-      </div>
     </section>
+    <div class="mini-footer">
+    </div>
