5fc76626a73232f1ee32e5498a8b96752cd651c6
semschilt <302862459@student.rocmondriaan.nl>
2017-01-26 16:16:10 +0100

    Update banner.php

--- templates/banner.php
+++ templates/banner.php
@@ -1,8 +1,1 @@
-<section class="lead-banner">
-  <img src="img/banner-1.jpg" />
-  <div class="banner-content">
-    <h1>JOHN DOE</h1>
-    <h2>FRONT-END DEVELOPER</h2>
-    <input type="submit" value="Learn more">
-  </div>
-</section>
+
