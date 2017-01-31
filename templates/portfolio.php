a2c4d43e3323595e14489e78db6c33576d254557
Sem van der Schilt <semvanderschilt@Sems-MacBook-Pro.local>
2017-01-31 14:06:36 +0100

    finished project

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,43 +1,42 @@
 <section class="portfolio border">
   <h2>Portfolio</h2>
   <p class="portfolio-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet,</p>
-  <ul class="portfolio-items">
-    <li>
-      <a href="/index.php">
-          <img class="portfolio-picture" src="img/1.jpg">
-        <span class="text-content">Place Name</span>
+  <ul class="portfolio-list">
+    <li class="portfolio-item">
+      <a href="">
+        <img class="portfolio-picture" src="img/alps-1.jpg">
+        <span class="text-content"><h3>lorem ipsum</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet</span>
       </a>
     </li>
-    <li>
-      <a href="/index.php">
-          <img class="portfolio-picture" src="img/2.jpg">
-        <span class="text-content">Place Name</span>
+    <li class="portfolio-item">
+      <a href="">
+        <img class="portfolio-picture" src="img/city-1.jpeg">
+        <span class="text-content"><h3>lorem ipsum</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet</span>
       </a>
     </li>
-    <li>
-      <a href="/index.php">
-          <img class="portfolio-picture" src="img/3.jpg">
-          <span class="text-content">Place Name</span>
+    <li class="portfolio-item">
+      <a href="">
+        <img class="portfolio-picture" src="img/hanamura.jpg">
+        <span class="text-content"><h3>lorem ipsum</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet</span>
       </a>
     </li>
-    <li>
-      <a href="/index.php">
-          <img class="portfolio-picture" src="img/4.png">
-          <span class="text-content">Place Name</span>
+    <li class="portfolio-item">
+      <a href="">
+        <img class="portfolio-picture" src="img/nature-1.jpg">
+        <span class="text-content"><h3>lorem ipsum</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet</span>
       </a>
     </li>
-    <li>
-      <a href="/index.php">
-          <img class="portfolio-picture" src="img/5.jpg">
-          <span class="text-content">Place Name</span>
+    <li class="portfolio-item">
+      <a href="">
+        <img class="portfolio-picture" src="img/raining-1.jpg">
+        <span class="text-content"><h3>lorem ipsum</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet</span>
       </a>
     </li>
-    <li>
-      <a href="/index.php">
-          <img class="portfolio-picture" src="img/6.jpg">
-          <span class="text-content">Place Name</span>
+    <li class="portfolio-item">
+      <a href="">
+        <img class="portfolio-picture" src="img/route-66.jpg">
+        <span class="text-content"><h3>lorem ipsum</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor porta euismod. Pellentesque ante nunc, scelerisque a purus sit amet</span>
       </a>
     </li>
   </ul>
 </section>
-
