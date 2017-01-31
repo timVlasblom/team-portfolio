75ea92d7f209c70d97d489e6122d72e09300e3d1
MaximeKoomen <maximekoomen@gmail.com>
2017-01-31 11:14:52 +0100

    Update main-nav.php

--- templates/main-nav.php
+++ templates/main-nav.php
@@ -1,9 +1,10 @@
 <nav class="navigation">
     <ul class="nav-bar">
-        <li class="nav-link"><a href="/home/">Home</a></li>
-        <li class="nav-link"><a href="/about/">About</a></li>
-        <li class="nav-link"><a href="/skills/">Skills</a></li>
-        <li class="nav-link"><a href="/portfolio/">Portfolio</a></li>
-        <li class="nav-link"><a href="/contact/">Contact</a></li>
+        <li class="nav-link"><a href="">Home</a></li>
+        <li class="nav-link"><a href="">About</a></li>
+        <li class="nav-link"><a href="">Skills</a></li>
+        <li class="nav-link"><a href="">Portfolio</a></li>
+        <li class="nav-link"><a href="">Contact</a></li>
     </ul>
 </nav>
+
