a07eb7ec54f2be7d47e64d2967beefda2ffb1ec6
MaximeKoomen <maximekoomen@gmail.com>
2017-01-31 11:12:07 +0100

    Update banner.php

--- templates/banner.php
+++ templates/banner.php
@@ -1,5 +1,4 @@
 <section class="banner border">
-  <img class="banner-picture" src="img/background.png">
     <div class="banner-content">
       <h1>John doe<span class="head-title">Frontend Developer</span></h1>
       <a class="banner-button" href="http://groenst.com/">Learn more</a>
